0812d92e5d8cbab689f3b8f84d5912b9a248e90a
Pâm Carla <pamycarla070@gmail.com>
2025-01-23 16:18:35 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -2,21 +2,20 @@ package sistema;
 
 public class Funcionario  extends Pessoa {
 	
-                         private int id_Funcionario;
                          private String cargo;
-			 private String  Lista_Registro;
-			 private double Salario_Base;
+			 private String lista_Registro;
+			 private double salario_Base;
 			 private String Conta_Bancaria;
 			 private String matricula;
 			
 			
-			 Funcionario(String cargo ,StringLista_Registro ,double Salario_Base,String Conta_Bancaria,String matricula ){
-				 
-				this.cargo=cargo;
-				this.Lista_Registro=Lista_Registro
-				this.Salario_Base=Salario_Base;
-				this.Conta_Bancaria=Conta_Bancaria;
-				this.matricula =matricula;
+			 Funcionario(String cargo, String lista_Registro, double salario_Base, String Conta_Bancaria, String matricula ){
+				super(nome, cpf, data_nascimento, endereco);
+				this.cargo = cargo;
+				this.lista_Registro = lista_Registro
+				this.salario_Base = salario_Base;
+				this.Conta_Bancaria = Conta_Bancaria;
+				this.matricula = matricula;
 			 }
 			 
 			//MÉTODOS
@@ -24,27 +23,30 @@ public class Funcionario  extends Pessoa {
 			 
 			 //MÉTODOS GET E SET
 			 
-			 public void setcargo(String cargo) {
-				 this.cargo=cargo;
+			 public void setCargo (String cargo) {
+				 this.cargo = cargo;
 			 }
-			 public String getcargo() {
-				 return cargo;
+			 public String getCargo() {
+				 return this.cargo;
 			 }
-			 public void setLista_Registro(String Lista_Registro) {
-					this.Lista_Registro=Lista_Registro;
+			 public void setLista_Registro(String lista_Registro) {
+				this.lista_Registro = lista_Registro;
 			}
 			 public double  getLista_Registro() {
-					 return Lista_Registro;
+				return this.lista_Registro;
 			}
-			 public void setSalario_Base(double Salario_Base) {
-				 this.Salario_Base=Salario_Base;
+			 public void setSalario_Base(double salario_Base) {
+				 this.salario_Base = salario_Base;
 			 }
 			 public String getConta_Bancaria() {
-				 return Conta_Bancaria;
+				 return this.Conta_Bancaria;
 			 }
-			 public void setmatricula (String matricula) {
-				 this.matricula=matricula;
+			 public void setMatricula (String matricula) {
+				 this.matricula = matricula;
 			 }
+			public String getMatricula(){
+				return this.matricula;
+			}
 	
 				 
 
