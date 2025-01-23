c5e36eb3a5906ed5897e16ab0007b26b0ba8447a
Thais dos Santos Figueiredo <figueiredothais652@gmail.com>
2025-01-23 17:19:51 -0300

    Update Conta_Bancaria.java

--- Conta_Bancaria.java
+++ Conta_Bancaria.java
@@ -1,9 +1,40 @@
 public abstract class Conta_Bancaria {
+	
 	private String numero_conta;
 	private String agencia;
 	private double sacar;
-	
 	public abstract void sacar();
-	
+		
+		Conta_Bancaria(String numero_conta,String agencia,double sacar){
 
+			this.numero_conta=numero_conta;
+			this.agencia=agencia;
+			this.sacar=sacar;
 }
+
+		public String getNumero_conta() {
+			return numero_conta;
+		}
+
+		public void setNumero_conta(String numero_conta) {
+			this.numero_conta = numero_conta;
+		}
+
+		public String getAgencia() {
+			return agencia;
+		}
+
+		public void setAgencia(String agencia) {
+			this.agencia = agencia;
+		}
+
+		public double getSacar() {
+			return sacar;
+		}
+
+		public void setSacar(double sacar) {
+			this.sacar = sacar;
+		}
+		
+	}
+	
