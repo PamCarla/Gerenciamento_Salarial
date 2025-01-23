27281c682760d04e07eb22356d1ab10517f736dd
Jeferson-Daniel <112097486+Jeferson-Daniel@users.noreply.github.com>
2025-01-23 18:02:44 -0300

    Update Pagamentos.java

--- Pagamentos.java
+++ Pagamentos.java
@@ -1,24 +1,24 @@
-public class Pagamentos implements Beneficio {
-     @Override
-     public double calcula_Beneficios(double salario_Base){
-          
-          
-          valor_hora_normal = salario_Base / horas_trabalhadas;
-          valor_hora_extra = valor_hora_normal * 1,5 * hora_extras;
+public class Pagamento implements Beneficio {
 
-          //comissao
-          //valor_comissao = vendas_realizada * 0.5 //5% percentual da comissao
+    @Override
+    public double calcula_Beneficios(double salario_Base) {
+        double horas_trabalhadas = 160; // Exemplo de horas trabalhadas
+        double valor_hora_normal = salario_Base / horas_trabalhadas;
 
-          salario_bruto = salario_Base + valor_hora_extra;
+        double hora_extras = 10; // Exemplo de horas extras trabalhadas
+        double valor_hora_extra = valor_hora_normal * 1.5 * hora_extras;
 
-          double inss = 0.11;
-          double irrf = 0.15;
-          //double vale_refeicao = 20;
+        // Cálculo do salário bruto
+        double salario_bruto = salario_Base + valor_hora_extra;
 
-          salario_liquido = salario_bruto - inss - irrf; // + vale_refeicao;
-
-
-
-     }
-
+        // Descontos
+        double inss = 0.11;
+        double irrf = 0.15;
+        //double vale_refeicao = 20;
+        // Cálculo do salário líquido
+        
+        double salario_liquido = salario_bruto - (salario_bruto * inss) - (salario_bruto * irrf); // + vale_refeicao;
+        
+        return salario_liquido;
+    }
 }
