c8cf7394f804e46bd03f7a8012f9f29814db928b
Thais dos Santos Figueiredo <figueiredothais652@gmail.com>
2025-01-23 17:09:39 -0300

    Update Gerenciamento_Salarial.java

--- Gerenciamento_Salarial.java
+++ Gerenciamento_Salarial.java
@@ -1,6 +1,6 @@
 public class Gerenciamento_Salarial {
      public static void main(String [] args){
-          Funcionario funcionario_1 = new Funcionario ;
+          Funcionario funcionario_1 = new Funcionario("joão ") ;
 
      }
 
