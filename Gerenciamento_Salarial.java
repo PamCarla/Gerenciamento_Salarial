261482bff7d99083032baf868453cb761db2c8d4
Jeferson-Daniel <112097486+Jeferson-Daniel@users.noreply.github.com>
2025-01-24 16:22:01 -0300

    Update Gerenciamento_Salarial.java

--- Gerenciamento_Salarial.java
+++ Gerenciamento_Salarial.java
@@ -1,79 +1,71 @@
-package sistema;
-
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 public class Gerenciamento_Salarial {
-
     public static void main(String[] args) {
-        // Coleta de dados com JOptionPane
-        String login = JOptionPane.showInputDialog("Digite o login do funcionário:");
-        String senha = JOptionPane.showInputDialog("Digite a senha do funcionário:");
-        String email = JOptionPane.showInputDialog("Digite o e-mail do funcionário:");
-        String contato = JOptionPane.showInputDialog("Digite o número de contato do funcionário:");
+        JTextField loginField = new JTextField();
+        JTextField senhaField = new JTextField();
+        JTextField emailField = new JTextField();
+        JTextField contatoField = new JTextField();
+        JTextField cargoField = new JTextField();
+        JTextField registroField = new JTextField();
+        JTextField salarioBaseField = new JTextField();
+        JTextField contaBancariaField = new JTextField();
+        JTextField matriculaField = new JTextField();
+        JTextField valorHoraNormalField = new JTextField();
+        JTextField valorHoraExtraField = new JTextField();
+        JTextField horasTrabalhadasField = new JTextField();
 
-        String cargo = JOptionPane.showInputDialog("Digite o cargo do funcionário:");
-        String lista_Registro = JOptionPane.showInputDialog("Digite o número de registro do funcionário:");
-        double salario_Base = 0;
-        try {
-            salario_Base = Double.parseDouble(JOptionPane.showInputDialog("Digite o salário base do funcionário:"));
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Por favor, insira um valor numérico válido para o salário base.");
-            return;
-        }
+        Object[] message = {
+            "Login:", loginField,
+            "Senha:", senhaField,
+            "E-mail:", emailField,
+            "Contato:", contatoField,
+            "Cargo:", cargoField,
+            "Registro:", registroField,
+            "Salário Base:", salarioBaseField,
+            "Conta Bancária:", contaBancariaField,
+            "Matrícula:", matriculaField,
+            "Valor Hora Normal:", valorHoraNormalField,
+            "Valor Hora Extra:", valorHoraExtraField,
+            "Horas Trabalhadas:", horasTrabalhadasField
+        };
 
-        String Conta_Bancaria = JOptionPane.showInputDialog("Digite o número da conta bancária:");
-        String matricula = JOptionPane.showInputDialog("Digite a matrícula do funcionário:");
-        
-        double valor_hora_normal = 0;
-        try {
-            valor_hora_normal = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor da hora normal:"));
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Por favor, insira um valor numérico válido para o valor da hora normal.");
-            return;
-        }
+        int option = JOptionPane.showConfirmDialog(null, message, "Cadastro de Funcionário", JOptionPane.OK_CANCEL_OPTION);
+        if (option == JOptionPane.OK_OPTION) {
+            try {
+                String login = loginField.getText();
+                String senha = senhaField.getText();
+                String email = emailField.getText();
+                String contato = contatoField.getText();
+                String cargo = cargoField.getText();
+                String registro = registroField.getText();
+                double salarioBase = Double.parseDouble(salarioBaseField.getText());
+                String contaBancaria = contaBancariaField.getText();
+                String matricula = matriculaField.getText();
+                double valorHoraNormal = Double.parseDouble(valorHoraNormalField.getText());
+                double valorHoraExtra = Double.parseDouble(valorHoraExtraField.getText());
+                int horasTrabalhadas = Integer.parseInt(horasTrabalhadasField.getText());
+                
+                double salarioBruto = salarioBase + (valorHoraExtra * horasTrabalhadas);
+                
+                Funcionario funcionario = new Funcionario(login, senha, email, contato, cargo, registro, salarioBase,
+                                                          contaBancaria, matricula, valorHoraNormal, valorHoraExtra,
+                                                          horasTrabalhadas, salarioBruto);
 
-        double valor_hora_extra = 0;
-        try {
-            valor_hora_extra = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor da hora extra:"));
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Por favor, insira um valor numérico válido para o valor da hora extra.");
-            return;
-        }
-
-        int horas_trabalhadas = 0;
-        try {
-            horas_trabalhadas = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de horas trabalhadas no mês:"));
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Por favor, insira um valor numérico válido para as horas trabalhadas.");
-            return;
-        }
-
-        double salario_bruto = salario_Base + (valor_hora_extra * horas_trabalhadas); // Exemplo de cálculo
-
-        // Criando um objeto Funcionario com os dados preenchidos
-        Funcionario funcionario = new Funcionario(login, senha, email, contato, cargo, lista_Registro, salario_Base, 
-                                                  Conta_Bancaria, matricula, valor_hora_normal, valor_hora_extra, 
-                                                  horas_trabalhadas, salario_bruto);
-
-        // Exibindo as informações do Funcionario
-        JOptionPane.showMessageDialog(null, "Funcionário cadastrado com sucesso!\n\n"
-                                          + "Cargo: " + funcionario.getCargo() + "\n"
-                                          + "Salário Base: " + funcionario.getSalario_Base() + "\n"
-                                          + "Horas Trabalhadas: " + funcionario.getHoras_trabalhadas() + "\n"
-                                          + "Salário Bruto: " + funcionario.getSalario_bruto());
-
-        // Oferecendo opções para o usuário
-        String[] options = {"Cadastrar outro funcionário", "Sair"};
-        int choice = JOptionPane.showOptionDialog(null, "Escolha uma opção:", "Opções",
-                                                  JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, 
-                                                  null, options, options[0]);
-
-        if (choice == 0) {
-            // Chama o método para cadastrar outro funcionário
-            main(null);  // Reinicia o programa
+                JOptionPane.showMessageDialog(null, "Funcionário cadastrado com sucesso!\n\n"
+                                              + "Cargo: " + funcionario.getCargo() + "\n"
+                                              + "Salário Base: " + funcionario.getSalario_Base() + "\n"
+                                              + "Horas Trabalhadas: " + funcionario.getHoras_trabalhadas() + "\n"
+                                              + "Salário Bruto: " + funcionario.getSalario_bruto());
+                
+                main(null);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Por favor, insira valores numéricos válidos.", "Erro", JOptionPane.ERROR_MESSAGE);
+                main(null);
+            }
         } else {
             JOptionPane.showMessageDialog(null, "Programa encerrado.");
-            System.exit(0); // Encerra o programa
+            System.exit(0);
         }
     }
 }
