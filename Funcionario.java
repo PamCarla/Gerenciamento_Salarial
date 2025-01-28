220faa6f2a54a1f263f5d49f404c039379c56781
Thais dos Santos Figueiredo <figueiredothais652@gmail.com>
2025-01-28 17:24:00 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -9,7 +9,7 @@ public class Funcionario  extends Usuario {
 	
 			
 			
-	Funcionario( String lohin,String senha,String email  String cargo,  String matricula, String cargo1, String lista_Registro, double salario_Base, String Conta_Bancaria1, String matricula1, double valor_hora_normal, double valor_hora_extra, int horas_trabalhadas, double salario_bruto){
+	Funcionario( String nome, String cpf,String data_nascimento,String endereco, String login,String senha,String email, String contato, String cargo,  String matricula, String cargo1, String lista_Registro, double salario_Base, String Conta_Bancaria1, String matricula1, double valor_hora_normal, double valor_hora_extra, int horas_trabalhadas, double salario_bruto){
 		super(null, null, null, null);
 		this.cargo = cargo;
 		this.salario_base = salario_Base;
