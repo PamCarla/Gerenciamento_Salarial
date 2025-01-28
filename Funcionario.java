62e6691df73b4c82e47b427929d2e54d3bf7e03d
Thais dos Santos Figueiredo <figueiredothais652@gmail.com>
2025-01-28 17:17:48 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,8 +1,6 @@
 public class Funcionario  extends Usuario {
 	private String cargo;
-	private String lista_registro;
 	private double salario_base;
-	private String Conta_Bancaria;
 	private String matricula;
 	private double valor_hora_normal;
 	private double valor_hora_extra;
@@ -11,12 +9,10 @@ public class Funcionario  extends Usuario {
 	
 			
 			
-	Funcionario(String cargo, String lista_registro, String Conta_Bancaria, String matricula, String cargo1, String lista_Registro, double salario_Base, String Conta_Bancaria1, String matricula1, double valor_hora_normal, double valor_hora_extra, int horas_trabalhadas, double salario_bruto){
+	Funcionario(String cargo,  String matricula, String cargo1, String lista_Registro, double salario_Base, String Conta_Bancaria1, String matricula1, double valor_hora_normal, double valor_hora_extra, int horas_trabalhadas, double salario_bruto){
 		super(null, null, null, null);
 		this.cargo = cargo;
-		this.lista_registro = lista_registro;
 		this.salario_base = salario_Base;
-		this.Conta_Bancaria = Conta_Bancaria;
 		this.matricula = matricula;
 		this.valor_hora_normal = valor_hora_normal;
 		this.valor_hora_extra = valor_hora_extra;
@@ -35,24 +31,14 @@ public class Funcionario  extends Usuario {
 		public String getCargo() {
 			return this.cargo;
 		}
-		public void setLista_Registro(String lista_registro) {
-			this.lista_registro = lista_registro;
-		}
-		public String getLista_Registro() {
-			return this.lista_registro;
-		}
+		
 		public void setSalario_Base(double salario_Base) {
 			this.salario_base = salario_Base;
 		}
 		public double getSalario_Base() {
 			return salario_base;
 		}
-		public void setConta_Bancaria(String conta_Bancaria) {
-			Conta_Bancaria = conta_Bancaria;
-		}
-		public String getConta_Bancaria() {
-			return this.Conta_Bancaria;
-		}
+		
 		public void setMatricula (String matricula) {
 			this.matricula = matricula;
 		}
